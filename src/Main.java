d082f8d23fb205e7daf0fdb3d15828b3c013879c
Mr. Linxed <hello@mrlinxed.com>
2023-12-21 20:28:21 +0100

    Implemented all win conditions

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     // TODO: Add replay-ability
     // TODO: Dynamic grid size (via question)
     // TODO: Add an optional 3rd player
+    // TODO: Add a read.me
 
     private static final Scanner cellInput = new Scanner(System.in);
 
@@ -91,23 +92,92 @@ public class Main {
     public static String getWinner() {
         String winner = "";
 
-        for(int y = 0; y < Main.gameState.length; y++) {
-            // Vertical check
-            String firstValue = "";
-            for (int x = 0; x < Main.gameState[y].length; x++) {
-                if(x == 0) {
-                    firstValue = Main.gameState[y][x];
-                } else if (!Objects.equals(firstValue, Main.gameState[y][x])) {
-                    firstValue = "";
+        for (String[] rowItems : Main.gameState) {
+            String firstItem = rowItems[0];
+
+            if(firstItem.equals(" ")){
+                continue;
+            }
+
+            boolean allSame = true;
+            for (int i = 1; i < rowItems.length; i++) {
+                if (!rowItems[i].equals(firstItem)) {
+                    allSame = false;
                     break;
-                } else {
-                    winner = firstValue;
                 }
             }
 
-            if(!firstValue.isEmpty()){
+            if(allSame) {
+                winner = firstItem;
+            }
+        }
+
+        if(!winner.isBlank()){
+            return winner;
+        }
+
+        String[] firstRowItems = Main.gameState[0];
+        for(int x = 0; x < firstRowItems.length; x++){
+            String topItem = firstRowItems[x];
+
+            if(topItem.equals(" ")){
+                continue;
+            }
+
+            boolean allSame = true;
+            //skip first row
+            for(int y = 1; y < Main.gameState.length; y++){
+                if (!Main.gameState[y][x].equals(topItem)) {
+                    allSame = false;
+                    break;
+                }
+            }
+
+            if(allSame) {
+                winner = topItem;
+            }
+        }
+
+        int gameSize = 3;
+        int[] topLeftBottomRightDiagonal = new int[gameSize];
+        int[] topRightBottomLeftDiagonal = new int[gameSize];
+
+        for(int x = 0; x < gameSize; x++) {
+            topLeftBottomRightDiagonal[x] = x;
+            topRightBottomLeftDiagonal[x] = (gameSize - 1) - x;
+        }
+
+        String firstItem = Main.gameState[0][0];
+        boolean allSame = true;
+        for (int y = 1; y < topLeftBottomRightDiagonal.length; y++) {
+            int x = topLeftBottomRightDiagonal[y];
+            if (!Main.gameState[y][x].equals(firstItem)) {
+                allSame = false;
                 break;
             }
+        }
+
+        if(allSame) {
+            return firstItem;
+        }
+
+        firstItem = Main.gameState[0][gameSize - 1];
+        allSame = true;
+        for (int y = 1; y < topRightBottomLeftDiagonal.length; y++) {
+            int x = topRightBottomLeftDiagonal[y];
+            System.out.print(y);
+            System.out.print(x);
+            System.out.print("=");
+            System.out.print(Main.gameState[y][x]);
+            System.out.println();
+            if (!Main.gameState[y][x].equals(firstItem)) {
+                allSame = false;
+                break;
+            }
+        }
+
+        if(allSame) {
+            return firstItem;
         }
 
         return winner;
